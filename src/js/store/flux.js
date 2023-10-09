9d5cfa0c9e176bdafada3cfcf03bb05dc126c757
Jesus D Santisteban <jesus.santisteban@gmail.com>
2023-10-09 22:08:33 +0000

    Fetched planets

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -2,6 +2,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			people: [],
+			planets: [],
+			favorites: [],
 			urlBase: "https://www.swapi.tech/api",
 		},
 		actions: {
@@ -23,6 +25,25 @@ const getState = ({ getStore, getActions, setStore }) => {
 				  console.log(error);
 				}
 			  },
+
+
+			  getPlanets: async () => {
+				let store = getStore();
+				try {
+					let response = await fetch(`${store.urlBase}/planets`);
+					let data = await response.json();
+					for (let planet of data.results) {
+						let response = await fetch(`${store.urlBase}/planets/${planet.uid}`);
+						let data = await response.json();
+
+						setStore({
+							planets: [...store.planets, data.result],
+						});
+					}
+				} catch (error) {
+					console.log(error)
+				}
+			  }
 			},
 		}
 	};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -8,7 +8,9 @@ export const Home = () => {
 	const { store } = useContext(Context);
 
   return (
-    <div className="container mt-5">
+	<>
+	
+    <div className="container mt-2">
       <h1 className="text-danger">Characters</h1>
       <div className="d-flex mycarrousel gap-2">
         {store.people.map((item) => {
@@ -34,11 +36,11 @@ export const Home = () => {
                 </p>
               </div>
               <div className="card-buttons d-flex justify-content-around mb-2">
-                <button type="button" class="btn btn-outline-primary">
+                <button type="button" className="btn btn-outline-primary">
                   Learn more!
                 </button>
                 <span>
-                  <i class="far fa-heart text-warning border border-warning fs-4 p-2"></i>
+                  <i className="far fa-heart text-warning border border-warning fs-4 p-2"></i>
                 </span>
               </div>
             </div>
@@ -46,5 +48,42 @@ export const Home = () => {
         })}
       </div>
     </div>
+
+	<div className="container mb-5">
+      <h1 className="text-danger">Planets</h1>
+      <div className="d-flex mycarrousel gap-2">
+        {store.planets.map((item) => {
+          return (
+            <div
+              key={item._id}
+              className="card border"
+              style={{ minWidth: "300px" }}
+            >
+              <img
+                src={`https://starwars-visualguide.com/assets/img/planets/${item.uid}.jpg`}
+                alt={item.properties.name}
+                className="card-img-top"
+              />
+              <div className="card-body">
+                <h5 className="card-title">{item.properties.name}</h5>
+                <p className="card-text">Population: {item.properties.population}</p>
+                <p className="card-text">
+                  Terrain: {item.properties.terrain}
+                </p>
+              </div>
+              <div className="card-buttons d-flex justify-content-around mb-2">
+                <button type="button" className="btn btn-outline-primary">
+                  Learn more!
+                </button>
+                <span>
+                  <i className="far fa-heart text-warning border border-warning fs-4 p-2"></i>
+                </span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+	</>
   );
 }
